4f6d4f873727967f1f1330212de9dce6c4ed6825
git-dru <mastercrypto.us@gmail.com>
2023-06-22 23:40:50 +0300

    add: event router

---
+++ Backend/controllers/event.controller.js
@@ -0,0 +1,20 @@
+async function create(req, res) {
+}
+
+async function getAll(req, res) {
+}
+
+async function update(req, res) {
+}
+
+async function remove(req, res) {
+}
+
+module.exports = {
+  create,
+  getAll,
+  update,
+  remove,
+};
+
+
