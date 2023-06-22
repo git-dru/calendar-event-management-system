4f37269d06e783d270343fb37a5f5d439f03cc8a
git-dru <mastercrypto.us@gmail.com>
2023-06-22 22:11:06 +0300

    feat: uuid dependency

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,3 +1,5 @@
+import { EventList } from "events/components";
+import { EventProvider } from "events/components/EventProvider";
 import "./App.css";
 import { Header } from "./shared/components";
 
@@ -5,7 +7,17 @@ function App() {
   return (
     <div className="App">
       <Header title="Calendar Event Management Application" />
-      <div className="mt-5"></div>
+      <div className="mt-5">
+        <EventProvider>
+          <div className="container-fluid">
+            <div className="d-flex flex-row">
+              <div className="col-sm-12">
+                <EventList />
+              </div>
+            </div>
+          </div>
+        </EventProvider>
+      </div>
     </div>
   );
 }
